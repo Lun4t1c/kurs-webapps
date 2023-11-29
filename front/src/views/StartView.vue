9061b4e43becb767a93ce77b5147ceff613b56fb
-Lunat1c- <arturgrabowski100@onet.pl>
2023-11-29 12:56:02 +0100

    Reworkdd StartView to composite api

--- front/src/views/StartView.vue
+++ front/src/views/StartView.vue
@@ -5,32 +5,25 @@
       Wprowadź numer
     </label>
     <input v-model="number" class="form-number clearfix" id="form-number" />
-    <div class="call-button" @click="call()">Zadzwoń teraz</div>
+    <div class="call-button" @click="call">Zadzwoń teraz</div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      number: "",
-    };
-  },
-  methods: {
-    async call() {
-      let responseStream = await fetch(
-        `${process.env.VUE_APP_SERVER_URL}/call`,
-        {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-          body: JSON.stringify({ number: this.number }),
-        }
-      );
-      let response = await responseStream.json();
-      this.$router.push({ name: "ringing", params: { callsId: response.id } });
+<script setup>
+import { ref } from 'vue';
+
+const number = ref('');
+
+const call = async () => {
+  const responseStream = await fetch(`${process.env.VUE_APP_SERVER_URL}/call`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
     },
-  },
+    body: JSON.stringify({ number: number.value }),
+  });
+
+  const response = await responseStream.json();
+  this.$router.push({ name: 'ringing', params: { callsId: response.id } });
 };
 </script>
