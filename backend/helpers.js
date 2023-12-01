7268c48db716e96cd6560e9fb959156c1a124d8d
-Lunat1c- <arturgrabowski100@onet.pl>
2023-12-01 11:09:23 +0100

    Mocking call

--- backend/helpers.js
+++ backend/helpers.js
@@ -9,12 +9,36 @@ class BridgeMock {
         FAILED: "FAILED"
     }
 
-    constructor() {
+    currentStatus = this.STATUSES.NEW;
 
+    constructor() {
+        this.mockCall();
     }
 
     async getStatus() {
-        return 'RINGING';
+        return this.currentStatus;
+    }
+
+    async mockCall() {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        this.currentStatus = this.STATUSES.READY;
+        console.log("Call status: ", this.STATUSES.READY);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        this.currentStatus = this.STATUSES.RINGING;
+        console.log("Call status: ", this.STATUSES.RINGING);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        this.currentStatus = this.STATUSES.CONNECTED;
+        console.log("Call status: ", this.STATUSES.CONNECTED);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        this.currentStatus = this.STATUSES.BRIDGED;
+        console.log("Call status: ", this.STATUSES.BRIDGED);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        this.currentStatus = this.STATUSES.ANSWERED;
+        console.log("Call status: ", this.STATUSES.ANSWERED);
     }
 }
 
